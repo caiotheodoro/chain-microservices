49f22f7785b349db3690c7c7cd09943d704fcdca
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-04-21 21:44:22 -0300

    feat: 🎸 protos + service

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -1,7 +1,11 @@
 use dotenvy::dotenv;
+use protos::auth::auth_server::AuthServer;
+use services::auth::Service;
 use services::db::connect_db;
+use tonic::transport::Server;
 
 mod models;
+mod protos;
 mod schema;
 mod services;
 
@@ -10,5 +14,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
     let mut db = connect_db();
+    let address = "[::1]:50051".parse()?;
+
+    Server::builder()
+        .add_service(AuthServer::new(Service::new(database)))
+        .serve(address)
+        .await?;
+
     Ok(())
 }
