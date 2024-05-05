0bed9264d5754cc5e23be1f947bb4bd87d7cf6b0
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-05-04 22:47:57 -0300

    feat: 🎸 cacahce integration

--- auth/src/services/redis/connect.rs
+++ auth/src/services/redis/connect.rs
@@ -3,7 +3,7 @@ use crate::services::authentication::messages::MessageService;
 extern crate redis;
 
 pub fn connect_redis() -> redis::Client {
-    let url = std::env::var("UPSTASH_REDIS_REST_URL").expect(MessageService::ERROR_REDIS_SETUP);
+    let url = std::env::var("REDIS_URL").expect(MessageService::ERROR_REDIS_SETUP);
 
     let client = redis::Client::open(url);
     let client = match client {
