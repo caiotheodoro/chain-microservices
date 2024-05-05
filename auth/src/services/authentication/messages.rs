62a56e969f5aa9e737805e52598f755b2b508bd6
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-05-04 23:04:50 -0300

    docs: ✏️ applicaiton error messages

--- auth/src/services/authentication/messages.rs
+++ auth/src/services/authentication/messages.rs
@@ -5,7 +5,8 @@ impl MessageService {
     pub const ERROR_CREATING_USER: &'static str = "Error creating user.";
     pub const USER_ALREADY_EXISTS: &'static str = "User already exists!";
     pub const ERROR_GENERATING_TOKEN: &'static str = "Error generating token for the user,";
-    pub const APP_KEY_MISSING: &'static str = "APP_KEY is missing from the environment variables.";
+    pub const APP_KEY_MISSING: &'static str =
+        "ACCESS_KEY is missing from the environment variables.";
     pub const INVALID_EMAIL_OR_PASSWORD: &'static str = "Invalid email or password.";
     pub const INVALID_USERNAME_OR_PASSWORD: &'static str = "Invalid username or password.";
     pub const UNAUTHENTICATED: &'static str = "Unauthenticated.";
@@ -13,4 +14,5 @@ impl MessageService {
     pub const INVALID_TOKEN: &'static str = "Invalid token.";
     pub const ERROR_GETTING_USER: &'static str = "Error getting user.";
     pub const ERROR_GETTING_USERS: &'static str = "Error getting users.";
+    pub const INVALID_REFRESH_TOKEN: &'static str = "Invalid refresh token.";
 }
